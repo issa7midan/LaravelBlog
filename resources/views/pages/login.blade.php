6650e47a8d5e999276e6c9b38ec53f367b9da30e
issa7midan <73063848+issa7midan@users.noreply.github.com>
2022-04-25 13:19:49 +0300

    WTF

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -65,7 +65,6 @@ https://templatemo.com/tm-551-stand-blog
               <li class="nav-item">
                 <a class="nav-link" href="contact">Contact Us</a>
               </li>
-       
             <li class="nav-item active">
             <a class="nav-link" href="login">Login</a>
           </li>
